6d549ab6737a5b8bc2f304f37e2a58a7871fce39
jkwn1 <jkwn1113@gmail.com>
2025-09-22 18:50:17 -0700

    Create 3522.cpp

---
+++ 3522.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long calculateScore(vector<string>& instructions, vector<int>& values) {
+        int n = (int) instructions.size();
+        long long score = 0;
+        int i = 0;
+        vector<bool> visited(n, false);
+        while (i >= 0 && i < n && !visited[i]) {
+            visited[i] = true;
+            if (instructions[i] == "add") {
+                score += values[i];
+                i++;
+            }
+            else {
+                i += values[i];
+            }
+        }
+        return score;
+    }
+};
