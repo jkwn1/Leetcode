768c40d86329d346096da6a0a640063c81a2c4b7
jkwn1 <jkwn1113@gmail.com>
2025-09-24 20:46:24 -0700

    Create 3683.cpp

---
+++ 3683.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int earliestTime(vector<vector<int>>& tasks) {
+        int ans = 300;
+        for (int i = 0; i < tasks.size(); i++) {
+            ans = min(ans, tasks[i][0] + tasks[i][1]);
+        }
+        return ans;
+    }
+};
