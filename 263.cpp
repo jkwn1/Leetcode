65ccc2055595bfdfa12decb96391f1d808f5e694
jkwn1 <jkwn1113@gmail.com>
2025-09-24 20:50:18 -0700

    Create 263.cpp

---
+++ 263.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        while (n > 1) {
+            if (n % 2 == 0) n /= 2;
+            else break;
+        }
+        while (n > 1) {
+            if (n % 3 == 0) n /= 3;
+            else break;
+        }
+        while (n > 1) {
+            if (n % 5 == 0) n /= 5;
+            else break;
+        }
+        return (n == 1);
+    }
+};
